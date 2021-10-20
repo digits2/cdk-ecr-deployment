17749c840ac33c7d4c4d6efc5102b75581ad48bb
Austin Dimmer <austin@effective-computing.com>
2021-10-20 18:58:59 +0000

    disable projen for testing integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-
 import * as child_process from 'child_process';
 import * as path from 'path';
 import * as ec2 from '@aws-cdk/aws-ec2';
